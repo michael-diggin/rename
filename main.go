f9bb6c22fc4bc118c6546e762bc3d03aafc7c460
Michael Diggin <michael.diggin@gmail.com>
2020-08-13 22:47:46 +0100

    updated to a fixed version

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 )
 
 func main() {
+	if runtime.GOOS == "windows" {
+		fmt.Println("Sorry, only works on unix/linux systems")
+		os.Exit(2)
+	}
+
 	args := os.Args[1:]
 	if len(args) != 2 {
 		fmt.Println("rename takes in two arguments")
@@ -18,23 +24,30 @@ func main() {
 	}
 
 	aliasCmd := "alias " + args[1] + "=\"" + args[0] + "\"\n"
+	rcPath := GetrcPath()
+	WriteToFile(rcPath, aliasCmd)
 
-	zshPath := os.Getenv("HOME") + "/.zshrc"
-	f, err := os.OpenFile(zshPath, os.O_APPEND|os.O_WRONLY, 0777)
+}
+
+// GetrcPath returns ~/.zshrc or ~/.bash_profile depending on the users shell
+func GetrcPath() string {
+	shell := os.Getenv("SHELL")
+	home := os.Getenv("HOME")
+	if shell[len(shell)-3:] == "zsh" {
+		return home + "/.zshrc"
+	}
+	return home + "/.bash_profile"
+}
+
+// WriteToFile appends a string to the given filepath
+func WriteToFile(path string, msg string) {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err := f.Write([]byte(aliasCmd)); err != nil {
+	defer f.Close()
+
+	if _, err := f.Write([]byte(msg)); err != nil {
 		log.Fatal(err)
 	}
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	//cmd := exec.Command("zsh", "-c", ". ./main.sh")
-
-	//	err = cmd.Run()
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
 }
